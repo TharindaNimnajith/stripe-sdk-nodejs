37ad0854466c513e4854fac34f07b77801bdda7c
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2023-06-25 12:22:17 +0530

    init

---
+++ app.js
@@ -0,0 +1,34 @@
+const stripe = require('stripe')('sk_test_51N9UNcJ163W9pHqjuzZbiyJlMWcXHCkUscXYOdxOMxiIY46JX2qMVhDEb9BV4dkBfkyEdkMcFsXCVDaSUU7ftJi9002pvXDnir')
+
+const express = require('express');
+
+const app = express();
+
+const port = "4242";
+app.set('port', port);
+
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'T-shirt',
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: 'http://localhost:4242/success',
+    cancel_url: 'http://localhost:4242/cancel',
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
+
+module.exports = app;
