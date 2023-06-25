c61becba13b1448b9afcf70f6cbc0d4f19b772f5
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2023-06-25 12:32:56 +0530

    init

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
-const stripe = require('stripe')('sk_test_51N9UNcJ163W9pHqjuzZbiyJlMWcXHCkUscXYOdxOMxiIY46JX2qMVhDEb9BV4dkBfkyEdkMcFsXCVDaSUU7ftJi9002pvXDnir')
+const express = require('express')
+const app = express()
 
-const express = require('express');
+const port = 4242
+app.set('port', port)
 
-const app = express();
-
-const port = "4242";
-app.set('port', port);
+const apiKey = 'sk_test_51N9UNcJ163W9pHqjuzZbiyJlMWcXHCkUscXYOdxOMxiIY46JX2qMVhDEb9BV4dkBfkyEdkMcFsXCVDaSUU7ftJi9002pvXDnir'
+const stripe = require('stripe')(apiKey)
 
 app.post('/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: 'T-shirt',
-          },
-          unit_amount: 2000,
+    line_items: [{
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: 'T-shirt'
         },
-        quantity: 1,
+        unit_amount: 2000
       },
-    ],
+      quantity: 1
+    }],
     mode: 'payment',
     success_url: 'http://localhost:4242/success',
-    cancel_url: 'http://localhost:4242/cancel',
-  });
+    cancel_url: 'http://localhost:4242/cancel'
+  })
 
-  res.redirect(303, session.url);
-});
+  res.redirect(303, session.url)
+})
 
-app.listen(port, () => console.log(`Listening on port ${port}!`));
+app.listen(port, () => console.log(`Listening on port ${port}`))
 
-module.exports = app;
+module.exports = app
